7a27ece1650c50f55f6ba718e8559a3428a73011
nyuta <pillowforneedles@gmail.com>
2020-09-09 17:42:21 +0300

    finished

--- src/components/loader/LoaderComponent.js
+++ src/components/loader/LoaderComponent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Loader from "react-loader-spinner";
+import styles from "./loader.module.css";
 
 function LoaderComponent() {
   return (
-    <div>
-      <Loader type="Grid" color="#e095ad" height={50} width={50} />
+    <div className={styles.wrapper}>
+      <Loader type="ThreeDots" color="#e095ad" height={50} width={50} />
     </div>
   );
 }
